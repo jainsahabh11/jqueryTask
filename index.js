0d5939ca25ad91b52ca7ed543ec6062761bb50a8
jainsahabh11 <shubham@segnotech.com>
2021-04-23 09:45:01 +0530

    sub heading add

--- index.js
+++ index.js
@@ -2,23 +2,39 @@ $(document).ready(function(){
  
     $(".btnsubmit").click(function(){
         var inp=$(".heading_name").val();
-        $(".maincontent").append($("<h1></h1>")).text(inp);
+       $(".heading_name").each(function(i){
+            $("#maincontent").append($("<div><h1>" + inp +"</h1></div>")); 
+            // <button type='button' class='close'><span aria-hidden='true'>&times;</span>
+            // console.log(i);
+// i++;
+        });
+        
+       
         var newOption = $('<option value="'+inp+'">'+inp+'</option>');
         $('.selectheading').append(newOption);
 
     });
     $(".btnsubmit2").click(function(){
         var inp2=$(".sub_name").val();
-        $(".maincontent").append($("<h3></h3>")).text(inp2);
+        $(".sub_name").each(function(){
+            
+            $("#maincontent>div:nth-child(1) ").append($("<div><h2>" + inp2 +"</h2></div>")); 
+            // $("#holder>div:nth-child(2)").append("<div>inserted div</div>");           
+        });
         var newOption2 = $('<option value="'+inp2+'">'+inp2+'</option>');
         $('.selectsub').append(newOption2);
     });
     $("btnsubmit3").click(function(){
-        var inp3=$(".inputarea").val();
-        console.log(inp3);
+       var lab=$(".lab").val();
+       var name=$(".name").val();
+       var ph=$(".ph").val();
+       var cls=$(".cls").val();
+       var val=$(".val").val();
+       var opt=$(".opt").val();
+       console.log(lab);
 
       
-    })
+    });
   
     
 });
