13f9b1aa0ffbd8e55f321927bc8411c43ae561e9
jainsahabh11 <shubham@segnotech.com>
2021-04-26 17:22:21 +0530

    add indexs and add new content

--- index.js
+++ index.js
@@ -4,66 +4,64 @@ $(document).ready(function () {
     $(".btnsubmit").click(function () {
         var inp = $(".heading_name").val();
 
-        $("main").append($('<div class="heading"><h1> ' + inp + '</h1></div>'));
+        $("main").append($('<div><h1> ' + inp + '</h1></div>'));
+
+        $(".selectheading option").remove();
+        $(".selectheading").append(' <option value="none" selected disabled hidden>--select--</option>');
+        $("main div h1").each(function (index, val) {
+            index = index + 1;
+            var aaa = $(this).text();
+            console.log(index, val);
+            $('.selectheading').append('<option value="' + index + '">' + aaa + '</option>');
 
 
-        $('.selectheading').append('<option value="' + inp + '">' + inp + '</option>');
+        });
+
 
     });
 
     $(".btnsubmit2").click(function () {
         var inp2 = $(".sub_name").val();
-
-        var sel = $(".selectheading")[0].selectedIndex;
         var selval = $(".selectheading").val();
-        // alert(selval);
-        $(".heading:nth-child(" + sel + ")").append($('<div class="subheading"><h2>' + inp2 + '</h2></div>'));
-
-        var newOption2 = $('<option value="' + selval + '">' + inp2 + '</option>');
-        $('.selectsub').append(newOption2, function () {
-            $(".selectheading").change(function () {
-                if ($(this).data('options') === undefined) {
-                    /*Taking an array of all options-2 and kind of embedding it on the select1*/
-                    $(this).data('options', $('.selectsub option').clone());
-                }
-                var cls = $(this).val();
-                var options = $(this).data('options').filter('[value=' + cls + ']');
-                $('.selectsub').html(options);
-            });
+        $("main div:nth-child(" + selval + ") h1").append($('<div><h2>' + inp2 + '</h2></div>'));
+        $(".selectsub option").remove();
+        $(".selectheading").append(' <option value="none" selected disabled hidden>--select--</option>');
+        $("main div h1 div h2").each(function (index, val) {
+            index = index + 1;
+            var aaa = $(this).text();
+            console.log(index, val);
+            $('.selectsub').append('<option value="' + index + '">' + aaa + '</option>');
         });
 
 
 
-        // var $select1 = $('.selectheading').val(),
-        //     $select2 = $('.selectsub').val(),
-        //     $options = $select2.find('option');
 
-        // $select1.change(function () {
-        //     // console.log('change');
-        //     $select2.html($options.filter('[value="' + selval + '"]'));
+        // var sel = $(".selectheading")[1].selectedIndex;
+        // var headingVal = $(".selectheading")[1].value;
+        // console.log("hello", headingVal, sel);
 
-        // }).trigger('change');
+        // $(".heading:nth-child(" + sel + ")").append($('<div class="subheading"><h2>' + inp2 + '</h2></div>'));
 
-        // $(".selectsub").children('option:gt(0)').hide();
-        // $(".selectheading").change(function () {
-        //     $(".selectsub").children('option').hide();
-        //     $(".selectsub").children("option[value^=" + $(this).val() + "]").show()
-        // })
+        // var newOption2 = $('<option value="' + headingVal + '">' + inp2 + '</option>');
+        // $('.selectsub').append(newOption2);
+    });
+
+    $(".selectheadingform").change(function () {
+        // if ($(this).data('options') === undefined) {
+        //     /*Taking an array of all options-2 and kind of embedding it on the select1*/
+        //     $(this).data('options', $('.selectsub option').clone());
+        // }
+        var id = $(this).val();
+        $('.selectsub option[value!=' + id + ']').remove();
+        $(this).data('options', $('.selectsub option')).each(function () {
+            var options = $(this).data('options').filter('[value=' + id + ']');
+            $('.selectsub').html(options);
+        });
 
 
     });
-
-    // $(".selectheading").change(function () {
-    //     var inp2 = $(".sub_name").val();
-    //     var newOption2 = $('<option value="' + inp2 + '">' + inp2 + '</option>');
-    //     $('.selectsub').append(newOption2);
-
-    // })
-
-
-
-
     $(".btnsubmit3").click(function () {
+
         var lab = $(".lab").val();
         var name = $(".name").val();
         var ph = $(".ph").val();
