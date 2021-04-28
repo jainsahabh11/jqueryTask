79ee0a569c5cccd15926c28d60afd7b7169f387a
jainsahabh11 <shubham@segnotech.com>
2021-04-28 16:01:49 +0530

    all select input fields work but select option input is not working

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 $(document).ready(function () {
 
-    $(".btnsubmit").click(function () {
+    $(".btnsubmit").click(function (e) {
+
+        e.preventDefault();
         var inp = $(".heading_name").val();
 
         $("main").append($('<div><h1> ' + inp + '</h1></div>'));
@@ -26,24 +28,26 @@ $(document).ready(function () {
 
         });
 
-
+        $('.myform')[0].reset();
     });
 
 
-    $(".btnsubmit2").click(function () {
+
+    $(".btnsubmit2").click(function (e) {
+        e.preventDefault();
         var inp2 = $(".sub_name").val();
         var selval = $(".selectheading").val();
         // console.log(selval, inp2);
         $("main div:nth-child(" + selval + ")").append('<section><h2>' + inp2 + '</h2></section>');
 
-
+        $('.myform2')[0].reset();
     });
     $(".selectheadingform").change(function () {
-        var selval = $(this).val();
+        var selval1 = $(this).val();
 
         $('.selectsubform option').remove();
         $(".selectsubform").append(' <option value="none" selected disabled hidden>--select--</option>');
-        $("main div:nth-child(" + selval + ") section").each(function (index) {
+        $("main div:nth-child(" + selval1 + ") section h2").each(function (index) {
             index = index + 1;
             var inp3 = $(this).text();
 
@@ -54,7 +58,8 @@ $(document).ready(function () {
     });
 
 
-    $(".btnsubmit3").click(function () {
+    $(".btnsubmit3").click(function (e) {
+        e.preventDefault();
         var label = $(".lab").val();
         var name = $(".name").val();
         var placeholder = $(".ph").val();
@@ -65,14 +70,56 @@ $(document).ready(function () {
 
         var headval = $(".selectheadingform").val();
         var subval = $(".selectsubform").val();
-        console.log(headval, subval);
-        // $("main div h1 section:nth-child(" + subval + ")").append(' <label for="emails">Enter email addresses:</label>');
-        $("main div:nth-child(" + headval + ") section:nth-child(" + subval + ")").append('<label for="name">' + label + '</label>');
+
+        var fos = $("main div:nth-child(" + headval + ") section:nth-of-type(" + subval + ")");
+
+        var fs = $('.formstatic').val();
+        var arr = $(".opt").val().split(",");
+
+
+        if (fs == 'radio') {
+            $.each(arr, function (i) {
+
+                $(fos).append('<p><input type="' + fs + '"  name="' + name + '" value="' + value + '""class="' + cls + '"> <label>' + arr[i] + ' </label></p>'
+                );
+            });
+
+
+        }
+
+        else if (fs == 'textarea') {
+            $.each(arr, function (j) {
+
+                $(fos).append('<p><input type="' + fs + '"  name="' + name + '" value="' + value + '"class="' + cls + '"> <label>' + arr[j] + ' </label></p>'
+                );
+            });
 
 
 
+        } else if (fs == 'select') {
+            var selc = '<p><select class="' + cls + '"></select></p>'
+            console.log(selc, '1111111111')
+            $.each(arr, function (k) {
+                $('<option value="' + value + '">' + arr[k] + '</option>').appendTo(selc);
+            });
+            $(fos).append(selc);
+
+        }
+        else if (fs == 'checkbox') {
+            $.each(arr, function (l) {
+
+                $(fos).append('<p><input type="' + fs + '"  name="' + name + '" value="' + value + '"class="' + cls + '"> <label>' + arr[l] + ' </label></p>'
+                );
+            });
+
+        }
+        else {
+            $(fos).append('<p><label >' + label + '</label><input type="' + fs + '" name="' + name + '" placeholder="' + placeholder + '" value="' + value + '"class="' + cls + '"></p>');
+
+        }
 
 
+        $('.myform3')[0].reset();
     });
 
 
